abe848610421b4b2655f56ae68a8c515068e9609
Rahulsharmarainbow <rainbowcroprise@gmail.com>
2025-11-07 16:49:43 +0530

    dynamic pages added

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './css/globals.css'
 import App from './App.tsx'
 import Spinner from './views/spinner/Spinner.tsx'
+import { HelmetProvider } from 'react-helmet-async';
 
 
 createRoot(document.getElementById('root')!).render(
+    
 <Suspense fallback={<Spinner />}>
+<HelmetProvider>
         <App />
+        </HelmetProvider>
     </Suspense>
     ,
 )
